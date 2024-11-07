16dd83a84b8de71c26efaf61e40a511d103cce0c
Nandit <nanditsharma063@gmail.com>
2024-11-07 20:09:52 +0530

    new

--- client-back-end/src/controllers/user.controller.js
+++ client-back-end/src/controllers/user.controller.js
@@ -304,7 +304,7 @@ const deleteAccount = async (req, res) => {
   return res.status(200).json({
    success: true,
    message: "Account deleted successfully",
-   data: listingOfUsers,
+   data: user,
   });
  } catch (error) {
   return res.status(500).json({
